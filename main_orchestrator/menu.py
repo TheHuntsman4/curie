4ef890860e7bf0ea821d649803ae877b8cce720c
TheHuntsman4 <anikethvij464@gmail.com>
2024-02-22 14:17:12 +0530

    <fix>: [fixes the base functionality of the menu]

--- main_orchestrator/menu.py
+++ main_orchestrator/menu.py
@@ -4,6 +4,27 @@ from llm_agent import LLMApp
 from img_gen import ImageGenWidget
 from discord import DiscordWidget
 
+class ButtonWidget(QWidget):
+    def __init__(self, stacked_widget):
+        super(ButtonWidget, self).__init__()
+
+        self.init_ui(stacked_widget)
+
+    def init_ui(self, stacked_widget):
+        button_layout = QVBoxLayout(self)
+
+        curie_button = QPushButton('CURIE')
+        curie_button.clicked.connect(lambda: stacked_widget.setCurrentIndex(1))
+        button_layout.addWidget(curie_button)
+
+        img_gen_button = QPushButton('Image Gen')
+        img_gen_button.clicked.connect(lambda: stacked_widget.setCurrentIndex(2))
+        button_layout.addWidget(img_gen_button)
+
+        discord_button = QPushButton('Discord')
+        discord_button.clicked.connect(lambda: stacked_widget.setCurrentIndex(3))
+        button_layout.addWidget(discord_button)
+
 class SinglePageApp(QWidget):
     def __init__(self):
         super(SinglePageApp, self).__init__()
@@ -14,9 +35,13 @@ class SinglePageApp(QWidget):
         self.setWindowTitle('Single Page App')
         self.setGeometry(100, 100, 400, 900)
 
-        main_layout = QVBoxLayout()
+        main_layout = QVBoxLayout(self)
 
         self.stacked_widget = QStackedWidget()
+
+        # Create the button widget and add it to the stacked widget
+        button_widget = ButtonWidget(self.stacked_widget)
+        self.stacked_widget.addWidget(button_widget)
 
         curie_widget = LLMApp()
         img_gen_widget = ImageGenWidget()
@@ -26,21 +51,7 @@ class SinglePageApp(QWidget):
         self.stacked_widget.addWidget(img_gen_widget)
         self.stacked_widget.addWidget(discord_widget)
 
-        curie_button = QPushButton('CURIE')
-        curie_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
-        main_layout.addWidget(curie_button)
-
-        img_gen_button = QPushButton('Image Gen')
-        img_gen_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
-        main_layout.addWidget(img_gen_button)
-
-        discord_button = QPushButton('Discord')
-        discord_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(2))
-        main_layout.addWidget(discord_button)
-
         main_layout.addWidget(self.stacked_widget)
-        self.setLayout(main_layout)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
